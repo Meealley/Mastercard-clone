ceeb49d13c129c8e6896e79d2d1ae42d74195113
Oyewale <oyewalefavour424@gmail.com>
2023-05-13 15:00:08 +0100

    second changes

--- src/Components/Layout/WaysToPay/WapFeatures.jsx
+++ src/Components/Layout/WaysToPay/WapFeatures.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 import Aos from "aos";
 
 const WapFeatures = () => {
